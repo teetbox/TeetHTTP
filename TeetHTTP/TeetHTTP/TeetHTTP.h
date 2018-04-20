41bad2753444759286c4c59c53b59ff8736536f9
teetbox <teetbox@gmail.com>
2018-04-20 22:32:14 +0800

    Add project

---
+++ TeetHTTP/TeetHTTP/TeetHTTP.h
@@ -0,0 +1,19 @@
+//
+//  TeetHTTP.h
+//  TeetHTTP
+//
+//  Created by Tong Tian on 20/04/2018.
+//  Copyright © 2018 TeetBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TeetHTTP.
+FOUNDATION_EXPORT double TeetHTTPVersionNumber;
+
+//! Project version string for TeetHTTP.
+FOUNDATION_EXPORT const unsigned char TeetHTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TeetHTTP/PublicHeader.h>
+
+
